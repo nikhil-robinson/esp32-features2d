87f1acc7612da020e7426fcf0b6f5b07c43c902d
Nikhil Robinson <nikhil@bibox.in>
2024-07-25 17:05:17 +0530

    finaly works

--- features2d/components/modules/ai/cv_opencv_features2d.hpp
+++ features2d/components/modules/ai/cv_opencv_features2d.hpp
@@ -1,18 +1,18 @@
-/*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
 
 
-long int sysconf(int);
 void init_opencv_features2d();
 void test_opencv_features2d(uint8_t* data, int width, int heigth);
-void register_cv_2d_feature(const QueueHandle_t frame_i,
-                                 const QueueHandle_t event,
-                                 const QueueHandle_t result,
-                                 const QueueHandle_t frame_o,const bool camera_fb_return);
+void register_cv_2d_feature(QueueHandle_t frame_i,
+                                   QueueHandle_t event,
+                                   QueueHandle_t result,
+                                   QueueHandle_t frame_o = NULL,
+                                   const bool camera_fb_return = false);
